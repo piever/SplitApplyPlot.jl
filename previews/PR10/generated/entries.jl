88b56611d1b91297da6cc238ca6610a9c0666b6d
Documenter.jl <documenter@juliadocs.github.io>
2021-04-14 00:07:52 +0000

    build based on 3715ee2

---
+++ previews/PR10/generated/entries.jl
@@ -0,0 +1,97 @@
+# # AxisEntries
+#
+# The key ingredient for data representations are `AxisEntries`.
+#
+# ## The `AxisEntries` type
+#
+# An `AxisEntries` object is
+# made of four components:
+# - axis,
+# - entries.
+
+using SplitApplyPlot, CairoMakie
+resolution = (600, 600)
+fig = Figure(; resolution)
+ae = AxisEntries(
+    Axis(fig[1, 1]),
+    [
+        Entry(
+            Scatter,
+            arguments(rand(10), rand(10), color=rand(10), marker=fill("b", 10));
+            markersize = 15
+        ),
+        Entry(
+            Scatter,
+            arguments(rand(10), rand(10), color=rand(10), marker=fill("c", 10));
+            markersize = 15
+        ),
+    ],
+    arguments("weight", "height", color="age", marker="name"), #labels
+    arguments(
+        (0, 1) => identity,
+        (0, 1) => identity,
+        color=(0, 1) => identity,
+        marker=LittleDict("a" => :circle, "b" => :utriangle, "c" => :dtriangle), #scales
+    ),
+)
+plot!(ae)
+display(fig)
+AbstractPlotting.save("axisentries.svg", AbstractPlotting.current_scene()); nothing #hide
+
+# ![](axisentries.svg)
+#
+# # Generating `AxisEntries` objects
+#
+# Generating `AxisEntries` objects by hand is extremely laborious. SplitApplyPlot provides
+# a simple way to generate them from data.
+
+using RDatasets
+mpg = RDatasets.dataset("ggplot2", "mpg")
+resolution = (600, 600)
+fig = Figure(; resolution)
+ag = splitapplyplot!(
+    mappings -> Entry(Scatter, mappings),
+    fig,
+    mpg,
+    :Displ => automatic => "Displacement",
+    :Cty => automatic => "City miles",
+    color=:Cyl => categoricalscale => "Cylinders",
+)
+
+# This operation returns a grid of `AxisEntries` and plots them to the original figure:
+
+display(fig)
+AbstractPlotting.save("splitapplyplot.svg", fig); nothing #hide
+
+# ![](splitapplyplot.svg)
+
+# `layout_x` and `layout_y` can be used to return a less trivial grid of axis plots.
+resolution = (1200, 1200)
+fig = Figure(; resolution)
+ag = splitapplyplot!(
+    mappings -> Entry(Scatter, mappings),
+    fig,
+    mpg,
+    :Displ => automatic => "Displacement",
+    :Cty => automatic => "City miles",
+    color=:Cyl => categoricalscale => "Cylinders",
+    layout_x=:Drv => categoricalscale => "Drive train",
+    layout_y=:Fl => categoricalscale => "Fuel type",
+)
+
+# The figure looks as follows:
+
+display(fig)
+AbstractPlotting.save("splitapplyplot_grid.svg", fig); nothing #hide
+
+# ![](splitapplyplot_grid.svg)
+#
+# The figure can then be further cleaned up by working with the matrix of axes:
+
+hideinnerdecorations!(ag)
+linkaxes!(ag...)
+display(fig)
+AbstractPlotting.save("splitapplyplot_grid_clean.svg", fig); nothing #hide
+
+# ![](splitapplyplot_grid_clean.svg)
+
