d96b1bdc8e99774dc2ba203d25b24aa1e11175ea
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2021-04-17 13:15:14 +0200

    restructure data organization (#15)

--- src/entries.jl
+++ src/entries.jl
@@ -7,6 +7,55 @@ end
 Entry(plottype::PlotFunc, arguments; attributes...) =
     Entry(plottype, arguments, Dict{Symbol, Any}(attributes))
 
+struct Entries
+    entries::Vector{Entry}
+    labels::Arguments
+    scales::Arguments
+end
+
+Entries() = Entries(Entry[], arguments(), arguments())
+
+"""
+    Entries(iterator)
+
+Return a unique `Entries` object from an iterator of `Entries`. Scales and labels are combined.
+"""
+Entries(iterator) = foldl(merge!, iterator, init=Entries())
+
+function Base.merge!(e1::Entries, e2::Entries)
+    entries = append!(e1.entries, e2.entries)
+    labels = mergewith!((a, b) -> isempty(b) ? a : b, e1.labels, e2.labels)
+    scales = mergewith!(merge_scales, e1.scales, e2.scales)
+    return Entries(entries, labels, scales)
+end
+
+function compute_axes_grid(fig, e::Entries)
+    dict = Dict{NTuple{2, Any}, AxisEntries}()
+    layout_scales = (
+        layout_y=get(e.scales, :layout_y, LittleDict(1 => 1)),
+        layout_x=get(e.scales, :layout_x, LittleDict(1 => 1)),
+    )
+    grid_size = map(length, layout_scales)
+    axes_grid = map(CartesianIndices(Tuple(grid_size))) do c
+        i, j = Tuple(c)
+        axis = Axis(fig[i, j])
+        return AxisEntries(axis, Entry[], e.labels, e.scales)
+    end
+    for entry in e.entries
+        layout = map((:layout_y, :layout_x)) do sym
+            scale = layout_scales[sym]
+            col = get(entry.mappings, sym, nothing)
+            # without layout info, plot on all axes
+            return isnothing(col) ? (1:grid_size[sym]) : rescale(col, scale)[1]
+        end
+        for i in layout[1], j in layout[2]
+            ae = axes_grid[i, j]
+            push!(ae.entries, entry)
+        end
+    end
+    return axes_grid
+end
+
 """
     AxisEntries(axis::Union{Axis, Nothing}, entries::Vector{Entry}, labels, scales)
 
@@ -16,35 +65,14 @@ Each scale can be either an ordered dictionary (for discrete collections), such 
 such as `(0, 10) => log10`. Other scales may be supported in the future.
 """
 struct AxisEntries
-    axis::Union{Axis, Nothing}
+    axis::Axis
     entries::Vector{Entry}
     labels::Arguments
     scales::Arguments
 end
 
-to_entries(entries::AbstractVector{Entry}) = entries
-to_entries(entry::Entry) = [entry]
-
-function AxisEntries(axis::Union{Axis, Nothing}=nothing,
-                     entries::Union{AbstractVector{Entry}, Entry}=Entry[])
-    return AxisEntries(axis, to_entries(entries), arguments(), arguments())
-end
-AxisEntries(axis::Union{Axis, Nothing}, entry::Entry) = AxisEntries(axis, [entry])
-AxisEntries(entries::AbstractVector{Entry}) = AxisEntries(nothing, entries)
-
 AbstractPlotting.Axis(ae::AxisEntries) = ae.axis
-has_axis(ae::AxisEntries) = !isnothing(Axis(ae))
-
-function Base.merge!(ae1::AxisEntries, ae2::AxisEntries)
-    axis = isnothing(ae2.axis) ? ae1.axis : ae2.axis
-    entries = append!(ae1.entries, ae2.entries)
-    labels = mergewith!((a, b) -> isempty(b) ? a : b, ae1.labels, ae2.labels)
-    scales = mergewith!(merge_scales, ae1.scales, ae2.scales)
-    return AxisEntries(axis, entries, labels, scales)
-end
-
-Base.merge(ae::AxisEntries, aes::AxisEntries...) = foldl(merge!, (ae, aes...), init=AxisEntries())
-Base.merge(ae::AxisEntries, axis::Axis) = merge(ae, AxisEntries(axis))
+Entries(ae::AxisEntries) = Entries(ae.entries, ae.labels, ae.scales)
 
 function prefix(i::Int, sym::Symbol)
     var = (:x, :y, :z)[i]
@@ -52,13 +80,14 @@ function prefix(i::Int, sym::Symbol)
 end
 
 function AbstractPlotting.plot!(ae::AxisEntries)
-    has_axis(ae) || return
     axis, entries, labels, scales = ae.axis, ae.entries, ae.labels, ae.scales
     for entry in entries
         plottype, mappings, attributes = entry.plottype, entry.mappings, entry.attributes
         trace = map(rescale, mappings, scales)
         positional, named = trace.positional, trace.named
         merge!(named, attributes)
+        pop!(named, :layout_y, nothing)
+        pop!(named, :layout_x, nothing)
         plot!(plottype, axis, positional...; named...)
     end
     # TODO: support log colorscale
