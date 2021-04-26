b132553889c6b6abb001ed76b9da54fe59dfabf9
piever <pietro.vertechi@protonmail.com>
2021-04-26 17:24:45 +0200

    fix

--- docs/src/generated/statistical_plots.jl
+++ docs/src/generated/statistical_plots.jl
@@ -1,6 +1,6 @@
 # # Statistical plots
 
-using SplitApplyPlot, CairoMakie, CSV, HTTP
+using SplitApplyPlot, CairoMakie, CSV, HTTP, DataFrames
 
 url = "https://cdn.jsdelivr.net/gh/allisonhorst/palmerpenguins@433439c8b013eff3d36c847bb7a27fa0d7e353d8/inst/extdata/penguins.csv"
 penguins = dropmissing(CSV.read(HTTP.get(url).body, DataFrame, missingstring="NA"))
