0b604c440f32001ee5195f0a660f51dd80ba6e4d
piever <pietro.vertechi@protonmail.com>
2021-04-26 12:19:04 +0200

    fix lm

--- src/transformations/linear.jl
+++ src/transformations/linear.jl
@@ -6,6 +6,7 @@ LinearAnalysis(; kwargs...) = LinearAnalysis(Dict{Symbol, Any}(kwargs))
 
 add_intercept_column(x::AbstractVector{T}) where {T} = [ones(T, length(x)) x]
 
+# TODO: add multidimensional version
 function (l::LinearAnalysis)(le::Entry)
     return splitapply(le) do entry
         labels, mappings = map(getlabel, entry.mappings), map(getvalue, entry.mappings)
@@ -13,7 +14,14 @@ function (l::LinearAnalysis)(le::Entry)
         wts = get(mappings, :wts, similar(x, 0))
         npoints = get(l.options, :npoints, 200)
         interval = get(l.options, :interval, length(wts) > 0 ? nothing : :confidence)
-        lin_model = GLM.lm(add_intercept_column(x), collect(y), wts=wts)
+        dropcollinear = false
+        lin_model = try
+            GLM.lm(add_intercept_column(x), collect(y); wts, dropcollinear)
+        catch e
+            @warn "linear fit not possible"
+            nothing
+        end
+        isnothing(lin_model) && return Entry[]
         x̂ = range(extrema(x)..., length=npoints)
         pred = GLM.predict(lin_model, add_intercept_column(x̂); interval)
         if !isnothing(interval)
