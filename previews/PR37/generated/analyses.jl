bfcef1ba8c70995c37164b1e27f0af5ccc2c72f4
Documenter.jl <documenter@juliadocs.github.io>
2021-04-26 10:32:45 +0000

    build based on b4aec89

--- previews/PR37/generated/analyses.jl
+++ previews/PR37/generated/analyses.jl
@@ -94,4 +94,7 @@ df = (x=randn(30), y=randn(30), z=rand(["a", "b", "c"], 30))
 specs = data(df) * mapping(:x, :y, color=:z) * (linear() + visual(Scatter))
 draw(specs, axis=(; limits=(-2, 2, -2, 2)))
 
-#
+# ## Smoothing
+
+specs = data(df) * mapping(:x, :y, color=:z) * (smooth() + visual(Scatter))
+draw(specs, axis=(; limits=(-2, 2, -2, 2)))
