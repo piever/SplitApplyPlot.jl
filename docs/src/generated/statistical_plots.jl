ae4bb829fb3e9129d3c620f5307d86a0e9fb3a5a
piever <pietro.vertechi@protonmail.com>
2021-04-27 11:55:33 +0200

    add violin dodge examples

--- docs/src/generated/statistical_plots.jl
+++ docs/src/generated/statistical_plots.jl
@@ -4,6 +4,27 @@ using SplitApplyPlot, CairoMakie, PalmerPenguins, DataFrames
 
 penguins = dropmissing(DataFrame(PalmerPenguins.load()))
 
+data(penguins) * visual(Violin) *
+    mapping(:species, :bill_depth_mm, color=:sex, dodge=:sex) |> draw
+
+#
+
+plt = data(penguins) * visual(Violin)
+plt *= mapping(:species, :bill_depth_mm, color=:sex, side=:sex, dodge=:island)
+draw(plt, axis=(limits=((0.5, 3.5), nothing),))
+
+#
+
+data(penguins) * visual(BoxPlot) *
+    mapping(:species, :bill_depth_mm, color=:sex, dodge=:sex) |> draw
+
+#
+
+data(penguins) * visual(CrossBar) *
+    mapping(:species, :bill_depth_mm, color=:sex, dodge=:sex) |> draw
+
+#
+
 data(penguins) *
     mapping(:bill_length_mm, :bill_depth_mm, col=:sex) *
     visual(QQPlot, qqline=:fit) |> draw
